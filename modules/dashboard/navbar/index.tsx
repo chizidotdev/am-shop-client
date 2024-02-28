be181896b14d98969466d45a37672a198db6d818
Chizi Victor <chiziwokoma@gmail.com>
2024-02-28 06:55:00 +0100

    styles: make dashboard nav scrollable

--- modules/dashboard/navbar/index.tsx
+++ modules/dashboard/navbar/index.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { FaRegHeart } from "react-icons/fa";
 import { Button } from "@/ui/button";
+import { ScrollArea, ScrollBar } from "@/ui/scroll-area";
 import { DashboardSearch } from "./dashboard-search";
 import { Login } from "@/modules/auth/login";
 import { usePathname } from "next/navigation";
@@ -56,26 +57,29 @@ export function DashboardNav() {
 
       <div className="border-b">
         <div className="max-w-7xl mx-auto px-5">
-          <div className="flex items-center gap-3 md:gap-5">
-            {links.map(({ href, label }) => {
-              const isActive = pathname === href;
+          <ScrollArea>
+            <div className="flex items-center gap-3 md:gap-5">
+              {links.map(({ href, label }) => {
+                const isActive = pathname === href;
 
-              return (
-                <Link
-                  key={`${href}${label}`}
-                  href={href}
-                  className={cn(
-                    "text-sm font-medium transition-colors border-b-2 border-background text-muted-foreground",
-                    "flex items-center h-12 sm:h-14 px-3",
-                    isActive && "text-foreground border-b-2 border-primary",
-                    "hover:text-foreground hover:border-primary",
-                  )}
-                >
-                  {label}
-                </Link>
-              );
-            })}
-          </div>
+                return (
+                  <Link
+                    key={`${href}${label}`}
+                    href={href}
+                    className={cn(
+                      "text-sm font-medium transition-colors border-b-2 border-background text-muted-foreground",
+                      "flex items-center h-12 sm:h-14 px-3",
+                      isActive && "text-foreground border-b-2 border-primary",
+                      "hover:text-foreground hover:border-primary",
+                    )}
+                  >
+                    {label}
+                  </Link>
+                );
+              })}
+            </div>
+            <ScrollBar className="h-0" orientation="horizontal" />
+          </ScrollArea>
           {/* <DashboardSearch className="sm:hidden w-full mt-5" /> */}
         </div>
       </div>
@@ -86,5 +90,6 @@ export function DashboardNav() {
 const links = [
   { href: "/dashboard", label: "Dashboard" },
   { href: "/dashboard/products", label: "Products" },
-  { href: "/settings", label: "Settings" },
+  { href: "/dashboard/orders", label: "Orders" },
+  { href: "/dashboard/settings", label: "Settings" },
 ];
